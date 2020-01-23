9af9defcfff71a6557600b3824851243f90fff62
mio <a771644612@live.com>
2020-01-23 04:37:00 -0500

    Let the pcb listen all connections

--- src/core/tcp_in.c
+++ src/core/tcp_in.c
@@ -315,40 +315,42 @@ tcp_input(struct pbuf *p, struct netif *inp)
     /* Finally, if we still did not get a match, we check all PCBs that
        are LISTENing for incoming connections. */
     prev = NULL;
-    for (lpcb = tcp_listen_pcbs.listen_pcbs; lpcb != NULL; lpcb = lpcb->next) {
-      /* check if PCB is bound to specific netif */
-      if ((lpcb->netif_idx != NETIF_NO_INDEX) &&
-          (lpcb->netif_idx != netif_get_index(ip_data.current_input_netif))) {
-        prev = (struct tcp_pcb *)lpcb;
-        continue;
-      }
-
-      if (lpcb->local_port == tcphdr->dest) {
-        if (IP_IS_ANY_TYPE_VAL(lpcb->local_ip)) {
-          /* found an ANY TYPE (IPv4/IPv6) match */
-#if SO_REUSE
-          lpcb_any = lpcb;
-          lpcb_prev = prev;
-#else /* SO_REUSE */
-          break;
-#endif /* SO_REUSE */
-        } else if (IP_ADDR_PCB_VERSION_MATCH_EXACT(lpcb, ip_current_dest_addr())) {
-          if (ip_addr_cmp(&lpcb->local_ip, ip_current_dest_addr())) {
-            /* found an exact match */
-            break;
-          } else if (ip_addr_isany(&lpcb->local_ip)) {
-            /* found an ANY-match */
-#if SO_REUSE
-            lpcb_any = lpcb;
-            lpcb_prev = prev;
-#else /* SO_REUSE */
-            break;
-#endif /* SO_REUSE */
-          }
-        }
-      }
-      prev = (struct tcp_pcb *)lpcb;
-    }
+    lpcb = tcp_listen_pcbs.listen_pcbs;
+    prev = (struct tcp_pcb *)lpcb;
+//    for (lpcb = tcp_listen_pcbs.listen_pcbs; lpcb != NULL; lpcb = lpcb->next) {
+//      /* check if PCB is bound to specific netif */
+//      if ((lpcb->netif_idx != NETIF_NO_INDEX) &&
+//          (lpcb->netif_idx != netif_get_index(ip_data.current_input_netif))) {
+//        prev = (struct tcp_pcb *)lpcb;
+//        continue;
+//      }
+//
+//      if (lpcb->local_port == tcphdr->dest) {
+//        if (IP_IS_ANY_TYPE_VAL(lpcb->local_ip)) {
+//          /* found an ANY TYPE (IPv4/IPv6) match */
+//#if SO_REUSE
+//          lpcb_any = lpcb;
+//          lpcb_prev = prev;
+//#else /* SO_REUSE */
+//          break;
+//#endif /* SO_REUSE */
+//        } else if (IP_ADDR_PCB_VERSION_MATCH_EXACT(lpcb, ip_current_dest_addr())) {
+//          if (ip_addr_cmp(&lpcb->local_ip, ip_current_dest_addr())) {
+//            /* found an exact match */
+//            break;
+//          } else if (ip_addr_isany(&lpcb->local_ip)) {
+//            /* found an ANY-match */
+//#if SO_REUSE
+//            lpcb_any = lpcb;
+//            lpcb_prev = prev;
+//#else /* SO_REUSE */
+//            break;
+//#endif /* SO_REUSE */
+//          }
+//        }
+//      }
+//      prev = (struct tcp_pcb *)lpcb;
+//    }
 #if SO_REUSE
     /* first try specific local IP */
     if (lpcb == NULL) {
